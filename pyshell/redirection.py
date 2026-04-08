96b2a012238d9c8a684d55e27c550ccbc25e127c
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-08 16:09:08 +0300

    feat(redirection): parse > output redirection operator

--- pyshell/redirection.py
+++ pyshell/redirection.py
@@ -1,28 +1,34 @@
-@staticmethod
-def parse(args):
-    stdin_file = None
-    stdout_file = None
-    stderr_file = None
-    append_stdout = False
-    append_stderr = False
-    
-    new_args = []
-    i = 0
-    while i < len(args):
-        arg = args[i]
+# redirection.py
+"""Handle shell redirections: > , >> , < , 2> , &>"""
+
+
+class RedirectionParser:
+    """Parse and handle shell redirections"""
+    @staticmethod
+    def parse(args):
+        stdin_file = None
+        stdout_file = None
+        stderr_file = None
+        append_stdout = False
+        append_stderr = False
         
-        # Output redirection (overwrite): > filename
-        if arg == '>':
-            if i + 1 >= len(args):
-                print("Error: missing filename for >")
-                return None, None, None, None, False, False
-            stdout_file = args[i + 1]
-            append_stdout = False
-            i += 2
-            continue
+        new_args = []
+        i = 0
+        while i < len(args):
+            arg = args[i]
+            
+            # Output redirection (overwrite): > filename
+            if arg == '>':
+                if i + 1 >= len(args):
+                    print("Error: missing filename for >")
+                    return None, None, None, None, False, False
+                stdout_file = args[i + 1]
+                append_stdout = False
+                i += 2
+                continue
+            
+            else:
+                new_args.append(arg)
+                i += 1
         
-        else:
-            new_args.append(arg)
-            i += 1
-    
-    return new_args, stdin_file, stdout_file, stderr_file, append_stdout, append_stderr
+        return new_args, stdin_file, stdout_file, stderr_file, append_stdout, append_stderr
