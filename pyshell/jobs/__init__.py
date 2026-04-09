8b40228fc35ac7f74ad19cccb5b5f9f5bdf3dad2
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-09 10:43:04 +0300

    feat(jobs): create jobs directory structure

---
+++ pyshell/jobs/__init__.py
@@ -0,0 +1,7 @@
+"""Job control module for background processes, fg, bg, jobs, kill"""
+
+from .job import Job
+from .job_table import JobTable
+from .control import fg, bg, jobs_cmd, kill_cmd
+from .background import run_background
+from .foreground import run_foreground
